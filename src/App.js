b5b75ef8b824bf424b9e6c9c2e4fef428c1b0737
chanh-t <81384798+chanh-t@users.noreply.github.com>
2024-07-05 00:04:44 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css'; // Import your CSS file
-
+// cp -r build/* .
+// npm run build
+// npm run deploy
 function App() {
   return (
     <div className="App">
@@ -15,33 +17,49 @@ function App() {
 
       <main className="main">
         <section id="about" className="about-section">
-          <h1>About Me</h1>
+          <h1 className="section-header">About Me</h1>
           <div className="about-content">
             <img src={"./profile-photo.jpg"} alt="nothing here?" title='A photo of me in 2022 when I was an undergraduate' className="profile-photo" />
             <div className="about-text">
-              <p>I am a recent undergraduate at the University of Washington with a degree in Computer Science.
+              <p>
+              I am a recent undergraduate at the University of Washington with a degree in Computer Science.
               </p>
             </div>
           </div>
         </section>
         
         <section id="projects" className="projects-section">
-          <h1>Projects</h1>
+          <h1 className="section-header">Projects</h1>
+
           <div className="projects-list">
             <div className="project-item">
-              <h2>Budget (on Google Sheets)</h2>
+              <div class="header-container">
+                <h1 class="header1">Budget on Google Sheets (In Progress)</h1>
+                <h1 class="header2">June 2024 - Present</h1>
+              </div>
+              <h3>Description</h3>
               <p>
                 Keeps track of all your recent transactions and gives you useful information about your spending habits.
               </p>
 
               <h3>Software and Tools</h3>
               <ul class="custom-list">
-                <li>Languages: TypeScript</li>
+                <li>Languages: Google Script</li>
+              </ul>
+              <h3>Links</h3>
+              <ul class="custom-list">
+                <li>Link to sheet onced finished!</li>
               </ul>
             </div>
+
+
+
             <div className="project-item">
-              <h2 className="header2">Poker (On Roblox) </h2>
-              
+              <div class="header-container">
+                  <h1 class="header1">Poker On Roblox (In Progress)</h1>
+                  <h1 class="header2">June 2024 - Present</h1>
+              </div>
+              <h3>Description</h3>
               <p>A simple poker game made on Roblox inspired by PokerNow.</p>
 
               <h3>Software and Tools</h3>
